f6563a37848f76a7c57d815dbbdc8c840d8b47c5
Federico Cadena <73864330+FedericoCadena9@users.noreply.github.com>
2023-10-21 13:08:19 -0600

    Fix supabasejs

--- utils/supabase.js
+++ utils/supabase.js
@@ -1,14 +1,12 @@
 import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 import { createClient } from "@supabase/supabase-js";
 
-const clientSupabase = createClientComponentClient(
+export const clientSupabase = createClientComponentClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL,
   process.env.NEXT_PUBLIC_SUPABASE_SERVICE_ROLE_KEY
 );
 
-const supabase = createClient(
+export const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL,
   process.env.NEXT_PUBLIC_SUPABASE_SERVICE_ROLE_KEY
 );
-
-export default { clientSupabase, supabase };
